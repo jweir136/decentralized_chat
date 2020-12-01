ce4236fb427a8360fdcad9dcc6e743493d05f6e0
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-01 13:08:58 -0500

    add correct signature test

--- block_cryptography/src/digital_signing.rs
+++ block_cryptography/src/digital_signing.rs
@@ -1,6 +1,3 @@
-use ring::hmac::{Key, HMAC_SHA256, Tag, sign, verify};
-use ring::rand::SystemRandom;
-use ring::rand::SecureRandom;
 use ring::error::Unspecified;
 use ring::{
     rand,
@@ -14,7 +11,7 @@ pub fn generate_keys() -> std::result::Result<Ed25519KeyPair, Unspecified> {
     Ok(key_pair)
 }
 
-pub fn sign_data(key_pair: Ed25519KeyPair, data: &[u8]) -> Signature {
+pub fn sign_data(key_pair: &Ed25519KeyPair, data: &[u8]) -> Signature {
     key_pair.sign(data)
 }
 
@@ -27,3 +24,29 @@ pub fn verify_data(public_key_bytes: &[u8], data: &[u8], signature: Signature) -
 }
 
 // TODO: Add new tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn correct_signing_test_1() {
+        let tests: [&str; 3] = [
+            "test",
+            "hello world",
+            "hashing is fun"
+        ];
+
+        for test in &tests {
+            let keys: Ed25519KeyPair = generate_keys().unwrap();
+
+            let sig = sign_data(&keys, test.as_bytes());
+            match verify_data(keys.public_key().as_ref(), test.as_bytes(), sig) {
+                false => { panic!(); },
+                true => {},
+                _ => { panic!(); }
+            };
+        }
+    }
+
+    // TODO: Add incorrect signature testing.
+}
