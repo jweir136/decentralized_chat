45d9216d782b4fb9cf4080dc99c9e7d8a2c82f3b
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-01 12:49:57 -0500

    change the signature module to use public-private key signing

--- block_cryptography/src/digital_signing.rs
+++ block_cryptography/src/digital_signing.rs
@@ -2,81 +2,28 @@ use ring::hmac::{Key, HMAC_SHA256, Tag, sign, verify};
 use ring::rand::SystemRandom;
 use ring::rand::SecureRandom;
 use ring::error::Unspecified;
+use ring::{
+    rand,
+    signature::{self, KeyPair, Ed25519KeyPair, Signature},
+};
 
-pub fn generate_key() -> Result<Key, Unspecified> {
-    let mut key_val = [0u8; 48];
-    let rng = SystemRandom::new();
-    rng.fill(&mut key_val)?;
-
-    Ok(Key::new(HMAC_SHA256, &key_val))
+pub fn generate_keys() -> std::result::Result<Ed25519KeyPair, Unspecified> {
+    let rng = rand::SystemRandom::new();
+    let pkcs8_bytes = signature::Ed25519KeyPair::generate_pkcs8(&rng)?;
+    let key_pair = signature::Ed25519KeyPair::from_pkcs8(pkcs8_bytes.as_ref())?;
+    Ok(key_pair)
 }
 
-pub fn sign_data(key: &Key, data : &[u8]) -> Tag {
-    sign(&key, &data)
+pub fn sign_data(key_pair: Ed25519KeyPair, data: &[u8]) -> Signature {
+    key_pair.sign(data)
 }
 
-pub fn verify_data(key: &Key, data: &[u8], signature: &[u8]) -> Result<(), Unspecified> {
-    verify(&key, &data, signature)?;
-    Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use ring::hmac::Tag;
-    use super::*;
-
-    #[test]
-    fn key_generation_tests() {
-        for _ in 0..50 {
-            generate_key().unwrap();
-        }
-    }
-
-    #[test]
-    fn data_signing_test() {
-        let test_strings : [&str; 5] = [
-            "Test",
-            "Hello World",
-            "Hashing is fun",
-            "Decentralization is the future",
-            "a-?.123'"
-        ];
-
-        for string in &test_strings {
-            match generate_key() {
-                Ok(new_key) => {
-                    let key = new_key;
-                    sign_data(&key, &string.as_bytes());
-                },
-                _ => { panic!(); }
-            };
-       }
-    }
-
-    #[test]
-    fn data_vertification_test() {
-        let test_strings : [&str; 5] = [
-            "Test",
-            "Hello World",
-            "Hashing is fun",
-            "Decentralization is the future",
-            "a-?.123'"
-        ];
-
-        for string in &test_strings {
-            match generate_key() {
-                Ok(new_key) => {
-                    let key = new_key;
-                    let signature: Tag = sign_data(&key, &string.as_bytes());
-                    verify_data(&key, &string.as_bytes(), signature.as_ref()).unwrap();
-                },
-                _ => { panic!(); }
-            };
-       }
-    }
-
-    #[test]
-    fn data_vertification_failure_test() {
-        // TODO
+pub fn verify_data(public_key_bytes: &[u8], data: &[u8], signature: Signature) -> bool {
+    let peer_public_key = signature::UnparsedPublicKey::new(&signature::ED25519, public_key_bytes);
+    match peer_public_key.verify(data, signature.as_ref()) {
+        Ok(()) => { true },
+        _ => { false }
     }
 }
+
+// TODO: Add new tests
