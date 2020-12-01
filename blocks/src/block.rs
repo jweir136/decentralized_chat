28390aacd233bbe7b61b77c3947e545bf83dc3b8
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-01 13:50:23 -0500

    add hashing and signing methods

--- blocks/src/block.rs
+++ blocks/src/block.rs
@@ -1,54 +1,59 @@
-use std::io::{Read, Result};
-use ring::digest::Digest;
-use ring::hmac::Tag;
 use std::time::Instant;
+use ring::hmac::{Key, Tag};
+use ring::digest::{Digest, Context, SHA256};
+use ring::signature::{Ed25519KeyPair, Signature};
 
-use block_cryptography::hash_digest;
+use block_cryptography::hashing::hash_digest;
+use block_cryptography::digital_signing::{sign_data};
 
-trait Block : Read + Copy {
-    fn hash(&self) -> Result<Digest>;
-
-    fn last_hash(&self) -> Result<Digest>;
+trait Block {
+    fn is_correct(&self, key: &Key) -> bool;
 }
 
-pub struct Chatblock {
+struct Chatblock {
     to: String,
     from: String,
     time: Instant,
     msg: String,
-    hash: Digest,
-    last_hash: Digest,
-    signature:  Tag,
-    position: usize
+    hash: Option<Digest>,
+    last_hash: Option<Digest>,
+    signature: Option<Signature>,
+    pos: usize
 }
 
-pub struct Nullblock {
-    hash: Digest,
-    last_hash: Digest
-}
+impl std::io::Read for Chatblock {
+    fn read(&mut self, buff: &mut [u8]) -> std::io::Result<usize> {
+        let mut n_read: usize = 0;
+        let string: &str = &format!("{}{}{}{:?}", self.to, self.from, self.msg, self.time);
 
-pub struct Genesisblock {
-    hash: Digest,
-    last_hash: Digest
-}
-
-impl Read for Chatblock {
-    fn read(&mut self, buff: &mut [u8]) -> Result<usize> {
-        let mut bytes_read: usize = 0;
-        let string: String = format!("{:?}{:?}", self.hash, self.last_hash);
-        
-        for i in self.position..buff.len() {
-            buff[i] = (&string).as_bytes()[self.position + i];
-            bytes_read += 1;
+        for i in self.pos..buff.len() {
+            buff[i] = string.as_bytes()[self.pos + i];
+            n_read += 1;
         }
 
-        self.position += bytes_read;
-        Ok(bytes_read)
+        self.pos += n_read;
+        Ok(n_read)
     }
 }
 
 impl Chatblock {
-    pub fn new(to: String, from: String, msg: String) -> Self {
-        // todo
+    pub fn new(to: String, from: String, msg: String, last_hash: Digest, keypair: &Ed25519KeyPair) -> Self {
+        let mut block: Self = Chatblock {
+            to: to,
+            from: from,
+            time: Instant::now(),
+            msg: msg,
+            hash: Option::None,
+            last_hash: Option::Some(last_hash),
+            signature: Option::None,
+            pos: 0
+        };
+
+        match hash_digest(&mut block, Context::new(&SHA256)) {
+            Ok(digest) => { block.hash = Option::Some(digest); },
+            _ => { panic!("Error: Cannot hash block"); }
+        };
+        block.signature = Option::Some(sign_data(&keypair, &format!("{:?}", block.hash).as_bytes()));
+        block
     }
 }
