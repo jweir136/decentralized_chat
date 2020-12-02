f42260a9d5a586f02d89cf7394e4ae38834507db
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-01 21:16:18 -0500

    change trait add method to use dynamic dispatch

--- blocks/src/blockchain.rs
+++ blocks/src/blockchain.rs
@@ -1,26 +1,6 @@
 use crate::block::{Block, Chatblock};
 
 pub trait Blockchain {
-    fn add<B: Block>(&mut self, block: B);
+    fn add(&mut self, block: Box<dyn Block>);
     fn get(&mut self, key: usize) -> Option<Box<dyn Block>>;
-}
-
-pub struct VectorBlockchain {
-    blocks: Vec<Chatblock>
-}
-
-impl VectorBlockchain {
-    pub fn new() -> Self {
-        VectorBlockchain { blocks: Vec::<Chatblock>::new() }
-    }
-}
-
-impl Blockchain for VectorBlockchain {
-    fn add<B: Block>(&mut self, block: B) {
-        self.blocks.push(block);
-    }
-
-    fn get(&mut self, key: usize) -> Option<Box<dyn Block>> {
-        self.blocks[key]
-    }
 }
