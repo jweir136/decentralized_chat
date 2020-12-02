4d6a8939cc61d264d0edf949b0cc9f48580ce874
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-01 20:44:44 -0500

    change to trait based implementation - it's more generic

--- blocks/src/blockchain.rs
+++ blocks/src/blockchain.rs
@@ -1,31 +1,7 @@
-use crate::block::Block;
+use crate::block::{Block};
 
-struct Blockchain {
-    blocks: Vec<Box<Block>>,
-    pos: usize
-}
-
-impl Blockchain {
-    pub fn new() -> Self {
-        Blockchain {
-            blocks: Vec::<Box<Block>>::new(),
-            pos: 0
-        }
-    }
-
-    pub fn add(&mut self, block: Box<Block>) {
-        self.blocks.push(block);
-    }
-
-    pub fn len(&self) -> usize {
-        self.blocks.len()
-    }
-
-    pub fn get(&self, idx: usize) -> Option<&Box<Block>> {
-        if idx >= self.blocks.len() {
-            Option::None
-        } else {
-            Option::Some(&self.blocks[idx])
-        }
-    }
+pub trait Blockchain {
+    fn add<B: Block>(&mut self, block: B);
+    fn delete(&mut self, key: usize) -> std::io::Result<()>;
+    fn get(&mut self, key: usize) -> Option<Box<dyn Block>>;
 }
