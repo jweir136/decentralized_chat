ce2b123451b2d0117b0f3bc62e9d26ff0821039d
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-01 22:01:35 -0500

    change VectorBlockchain to a Box and reference based method

--- blocks/src/blockchain.rs
+++ blocks/src/blockchain.rs
@@ -1,6 +1,32 @@
-use crate::block::{Block, Chatblock};
+use crate::block::{Block};
 
 pub trait Blockchain {
     fn add(&mut self, block: Box<dyn Block>);
-    fn get(&mut self, key: usize) -> Option<Box<dyn Block>>;
+    fn get(&mut self, key: usize) -> Option<&Box<dyn Block>>;
+}
+
+struct VectorBlockchain {
+    blocks: Vec<Box<dyn Block>>
+}
+
+impl VectorBlockchain {
+    pub fn new() -> Self {
+        VectorBlockchain {
+            blocks: Vec::<Box<dyn Block>>::new()
+        }
+    }
+}
+
+impl Blockchain for VectorBlockchain {
+    fn add(&mut self, block: Box<dyn Block>) {
+        self.blocks.push(block);
+    }
+
+    fn get(&mut self, key: usize) -> Option<&Box<dyn Block>> {
+        if key >= self.blocks.len() {
+            Option::None
+        } else {
+            Option::Some(&self.blocks[key])
+        }
+    }
 }
