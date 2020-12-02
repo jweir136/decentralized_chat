dbdaf39c0387951eaa9867a277a28999c51ba7e3
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-01 20:56:22 -0500

    create the VectorBlockchain implementation

--- blocks/src/blockchain.rs
+++ blocks/src/blockchain.rs
@@ -1,7 +1,26 @@
-use crate::block::{Block};
+use crate::block::{Block, Chatblock};
 
 pub trait Blockchain {
     fn add<B: Block>(&mut self, block: B);
-    fn delete(&mut self, key: usize) -> std::io::Result<()>;
     fn get(&mut self, key: usize) -> Option<Box<dyn Block>>;
+}
+
+pub struct VectorBlockchain {
+    blocks: Vec<Chatblock>
+}
+
+impl VectorBlockchain {
+    pub fn new() -> Self {
+        VectorBlockchain { blocks: Vec::<Chatblock>::new() }
+    }
+}
+
+impl Blockchain for VectorBlockchain {
+    fn add<B: Block>(&mut self, block: B) {
+        self.blocks.push(block);
+    }
+
+    fn get(&mut self, key: usize) -> Option<Box<dyn Block>> {
+        self.blocks[key]
+    }
 }
